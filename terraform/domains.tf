cfd449472fef3b9a9da1de7838221fd8a1d13347
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-11-20 07:14:09 -0500

    Updated the name for dns ip record

--- terraform/domains.tf
+++ terraform/domains.tf
@@ -6,7 +6,7 @@ resource "vultr_dns_domain" "cdrlut" {
   domain = "cdrl-ut.org"
 }
 
-resource "vultr_dns_record" "bastion_ip_record" {
+resource "vultr_dns_record" "autodock_ip_record" {
   domain = vultr_dns_domain.cdrlprojects.id
   name   = "autodock"
   data   = vultr_instance.autodock.main_ip
